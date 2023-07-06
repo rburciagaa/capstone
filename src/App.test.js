0c72de777f08060f4c6a2e73c14cea5c2ff37362
Ricardo Burciaga <rburciagaa@hotmail.com>
2023-07-06 17:58:14 -0600

    Organize some elements in index.html

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import BookingForm from "./components/Form";
+import { BrowserRouter as Router } from "react-router-dom";
+import "@testing-library/jest-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const mockProps = {
+  availableTimes: ["12:00 PM", "1:00 PM", "2:00 PM"],
+  updateTimes: jest.fn(),
+};
+
+test("renders all form fields", () => {
+  render(
+    <Router>
+        <BookingForm {...mockProps} />
+    </Router>
+  );
+  expect(screen.getByLabelText("First Name")).toBeInTheDocument();
+  expect(screen.getByLabelText("Last Name")).toBeInTheDocument();
+  expect(screen.getByLabelText("Email")).toBeInTheDocument();
+  expect(screen.getByLabelText("Phone Number")).toBeInTheDocument();
+  expect(screen.getByLabelText("Number of People")).toBeInTheDocument();
+  expect(screen.getByLabelText("Select Date")).toBeInTheDocument();
+  expect(screen.getByLabelText("Select Time")).toBeInTheDocument();
+  expect(screen.getByLabelText("Occasion")).toBeInTheDocument();
+  expect(screen.getByLabelText("Seating preferences")).toBeInTheDocument();
 });
